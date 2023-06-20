aab85cc0745408ad2de1f4733f99c22a308215e6
prathambongale <prathambongale@hotmail.com>
2023-06-20 08:40:53 +0200

    spring academy model 1 completed with spring data

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -20,14 +20,10 @@ class CashCardApplicationTests {
 
 	@Test
 	void shouldReturnACashWhenDataIsSaved() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
-
-		/**asserts that the request succeeded by checking for a 200 OK response status*/
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-		assertThat(response.getBody()).isBlank();
-
-		/**test that the response contains the correct values*/
-		/* DocumentContext documentContext = JsonPath.parse(response.getBody());
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
 
 		Number id = documentContext.read("$.id");
 		
@@ -35,6 +31,15 @@ class CashCardApplicationTests {
 
 		Double amount = documentContext.read("$.amount");
 
-		assertThat(amount).isEqualTo(123.45); */
+		assertThat(amount).isEqualTo(123.45);
+	}
+
+	@Test
+	void shouldNotReturnACashWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		/**asserts that the request succeeded by checking for a 200 OK response status*/
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
 	}
 }
